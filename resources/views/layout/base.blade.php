879f93ecb5142f2b9516ea8dec1f9a0d38c80b75
David <advitameternam@gmail.com>
2021-01-26 11:31:18 +0100

    initial commit change homepage and initialize tailwind

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,11 @@
+  <!doctype html>
+  <head>
+    <!-- ... --->
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+  <body>
+      <h1 class="text-blue-500 text-lg font-bold text-center">Bienvenue sur la première page</h1>
+  </body>
+  </html>
