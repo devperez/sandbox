eaca56ecf89f1c8187e37eab38f07e45cdc00629
David <advitameternam@gmail.com>
2021-01-26 17:35:08 +0100

    login operational

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,20 +6,20 @@
         <form class="text-center pt-5" action="{{route('register') }}" method="post">
             @csrf
             <div class="mb-4">
-                <label for="name">Your name :</label>
-                <input class="border-2 bg-gray-200 w-full h-8" id="name" name="name" type="text" placeholder="Your name" value="{{old('name')}}">
+                <label for="name" class="sr-only">Your name :</label>
+                <input class="border-2 bg-gray-200 w-full p-4 rounded-lg" id="name" name="name" type="text" placeholder="Your name" value="{{old('name')}}">
             </div>
             <div class="mb-4">
-                <label for="email">Your email:</label>
-                <input class="border-2 bg-gray-200 w-full h-8" type="email" id="email" name="email" placeholder="Your email" value="{{old('email') }}">
+                <label class="sr-only" for="email">Your email:</label>
+                <input class="border-2 bg-gray-200 w-full p-4 rounded-lg" type="email" id="email" name="email" placeholder="Your email" value="{{old('email') }}">
             </div>
             <div class="mb-4">
-                <label for="password">Your password :</label>
-                <input class="border-2 bg-gray-200 w-full h-8" type="password" id="password" name="password" placeholder="Choose a password">
+                <label class="sr-only" for="password">Your password :</label>
+                <input class="border-2 bg-gray-200 w-full p-4 rounded-lg" type="password" id="password" name="password" placeholder="Choose a password">
             </div>
             <div class="mb-4">
-                <label for="password_confirmation">Confirm your password :</label>
-                <input class="border-2 bg-gray-200 w-full h-8" type="password" placeholder="Enter your password again">
+                <label class="sr-only" for="password_confirmation">Confirm your password :</label>
+                <input class="border-2 bg-gray-200 w-full p-4 rounded-lg" type="password" id="password_confirmation" name="password_confirmation" placeholder="Enter your password again">
             </div>
             <button class="bg-blue-600 text-white p-4 w-full rounded" type="submit">Register</button>
         </form>
